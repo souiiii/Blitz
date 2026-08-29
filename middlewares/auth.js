17ca7c3adda46b7959da3f42be59749991941db8
souiiii <b423037@iiit-bh.ac.in>
2026-08-29 15:18:44 +0530

    auth middlewares added

---
+++ middlewares/auth.js
@@ -0,0 +1,21 @@
+async function authSoft(req, res, next) {
+  req.user = null;
+  const token = req.cookies?.token;
+
+  if (!token) return next();
+
+  try {
+    const user = getUser(token);
+    req.user = user;
+    return next();
+  } catch (err) {
+    res.clearCookie();
+    return res.status(401).json({ err: "Invalid user" });
+  }
+}
+
+async function authHard(req, res, next) {
+  if (!req.user) return res.status(401).json({ err: "Unauthenticated user" });
+
+  return next();
+}

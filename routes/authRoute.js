98c7090245fb6cfcf787936eb6e62b9976ae6210
souiiii <b423037@iiit-bh.ac.in>
2026-08-29 15:59:20 +0530

    completed the login and signup routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,62 @@
 import Router from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import { setUser } from "../services/auth.js";
 
 const router = Router();
 
-router.post("/signup", async (req, res) => {});
+router.post("/signup", async (req, res) => {
+  const { email, password, name } = req.body;
 
-router.post("/login", async (req, res) => {});
+  if (!email || !password || !name)
+    return res.status(400).json({ err: "Invalid request" });
+
+  name = name.trim().toLowerCase();
+  email = email.trim().toLowerCase();
+
+  if (!validator.isEmail(email))
+    return res.status(400).json({ err: "Invalid email format" });
+
+  const user = await User.find({ email });
+
+  if (user) return res.status(400).json({ err: "User already exists" });
+
+  const hashedPass = bcrypt.hash(password, 10);
+
+  await User.create({ name, email, password: hashedPass });
+
+  return res.status(201).json({ msg: "Account created" });
+});
+
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || !name)
+    return res.status(400).json({ err: "Invalid request" });
+
+  email = email.trim().toLowerCase();
+  if (!validator.isEmail(email))
+    return res.status(400).json({ err: "Invalid email format" });
+
+  const user = await User.find({ email });
+
+  if (!user) return res.status(400).json({ err: "Account not found" });
+
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(400).json({ err: "Invalid credentials" });
+
+  try {
+    const token = setUser({ id: user._id });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.production || false,
+      sameSite: none,
+    });
+
+    return res.status(200).json({ msg: "logged in" });
+  } catch (err) {
+    return res.status(500).json({ err: "Problem signing in" });
+  }
+});
 
 export default router;

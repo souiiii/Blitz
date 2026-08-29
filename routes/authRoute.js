7709796d780c007146b5cf9266a4e804f70e1716
souiiii <b423037@iiit-bh.ac.in>
2026-08-29 15:08:17 +0530

    created two routes user and links

---
+++ routes/authRoute.js
@@ -0,0 +1,9 @@
+import Router from "express";
+
+const router = Router();
+
+router.post("/signup", async (req, res) => {});
+
+router.post("/login", async (req, res) => {});
+
+export default router;
---
+++ routes/linkRoute.js
@@ -0,0 +1,9 @@
+import Router from "express";
+
+const router = Router();
+
+router.get("/:link", async (req, res) => {});
+
+router.post("/:original", async (req, res) => {});
+
+export default router;
